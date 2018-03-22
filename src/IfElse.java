b30a1e00936aa50a49722d8856427a4ddde013aa
Stas <maikydyk@mail.ru>
2018-03-23 00:31:33 +0300

    Работа в универе 21.03

---
+++ src/IfElse.java
@@ -0,0 +1,43 @@
+/**
+ * Created by Stas on 23.03.2018.
+ */
+public class IfElse {
+    public static void main(String[] args) throws InterruptedException {
+/*        // тернальный оператор
+        int num = 13;
+        String type;
+        type = num % 2 == 0 ? "even" : "odd";
+        // condition ?  "сли условие true" : 'если условие false"
+        System.out.println(type);
+
+        //циклы
+        for(int i = 0; i <= 10; i++){
+            if(i % 2 != 0){
+                System.out.println(i);
+            }
+        }
+
+        System.out.println("+++++++++");
+
+        for(int i = 1; i <= 10; i+=2){
+            System.out.println(i);
+        }
+        System.out.println("------------------");
+        int j = 5;
+        do{
+            System.out.println(j);
+            j--;
+            //Thread.sleep(1000);
+        }
+        while (j>=1);
+        System.out.println("-+000000000000");*/
+
+        // в переменной х хранится двухзначное число. Создать программу, вычисляющую сумму цифр числа n
+/*        int n = 751;
+        int sum = n % 100 / 10 + n % 100 % 10 + n / 100;
+        System.out.println(sum);*/
+
+        // дано число n, найти сумму цифр этого числа
+
+    }
+}
