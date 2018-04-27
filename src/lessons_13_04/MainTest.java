6cb9024a36a48e923905b3bdacf37c7f4c449c07
Stas <maikydyk@mail.ru>
2018-04-27 15:39:19 +0300

    Добавил очередь

--- src/lessons_13_04/MainTest.java
+++ src/lessons_13_04/MainTest.java
@@ -7,16 +7,16 @@ import java.util.LinkedList;
 public class MainTest {
     public static void main(String[] args) {
 
-        // Задание с планетами
+    /*    // Задание с планетами
         Planets[] planets = Planets.values();
         for(int i = 0; i < planets.length; i ++){
             System.out.println(planets[i] + " " + " масса : " +  planets[i].getMass()+ " "
                     + " радиус : " +  planets[i].getRasius()+ " "
                     + " радиус орбиты " + planets[i].getRadiusOrbita());
-        }
+        }*/
         //--------------------------------------------------------
 
-        // Задание с LinkedList
+/*        // Задание с LinkedList
         lessons_13_04.LinkedList newList = new lessons_13_04.LinkedList();
 
         newList.addLast("Firts Element");
@@ -31,9 +31,9 @@ public class MainTest {
         newList.addFirst("Zero");
         for(int i = 0; i < newList.size(); i++){
             System.out.println(newList.getElementByIndex(i));
-        }
+        }*/
 
-        // Задание Stack
+/*        // Задание Stack
         MyStack myStack = new MyStack(10);
         myStack.push("one");
         myStack.push("two");
@@ -53,6 +53,19 @@ public class MainTest {
         System.out.println(myStack1.pop());
         System.out.println(myStack1.pop());
         System.out.println(myStack1.pop());
+   */
+
+        MyQueue<String> queue = new MyQueue<>(10);
+        for(int i = 0; i < 10; i++){
+            System.out.println(queue.offer("Строка: " + i));
+        }
+
+        for(int i = 0; i < 3; i++){
+            System.out.println(queue.remove());
+        }
+
+        System.out.println(queue.peek());
     }
+
 
 }
