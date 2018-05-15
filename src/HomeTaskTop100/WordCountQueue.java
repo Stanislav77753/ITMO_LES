7fad19b1b166a96181bbc764d2e6a277a1127299
Stas <maikydyk@mail.ru>
2018-05-15 15:36:18 +0300

    TOP100 через Queue

--- src/HomeTaskTop100/WordCountQueue.java
+++ src/HomeTaskTop100/WordCountQueue.java
@@ -1,22 +1,31 @@
 package HomeTaskTop100;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
-public class WordCount {
+public class WordCountQueue {
     private static Map<String, Integer> result = new HashMap<>();
+    private static final String STOP = new String("stop");
     public static void main(String[] args) throws IOException, InterruptedException {
         long start = System.currentTimeMillis();
         MyValueComp mvc = new MyValueComp(result); // создали объект Компаратор для сортировки по значению
         TreeMap<String, Integer> treeMap = new TreeMap<>(mvc);
-
-        // Поток чтения книги
         InputStream in = WordCount.class.getClassLoader().getResourceAsStream("war.txt");
-        BufferedReader  rdr = new BufferedReader(new InputStreamReader(in));
+        BufferedReader rdr = new BufferedReader(new InputStreamReader(in));
         List<String> lines = rdr.lines().collect(toList());
+        int cpus = Runtime.getRuntime().availableProcessors();
+        List<WordCountQueue.WordCountThread> workers = new ArrayList<>();
+        BlockingQueue<String> linesQueue = new LinkedBlockingQueue();
+
         // Поток чтения файла со словами, которые надо исключить
         InputStream in2 = WordCount.class.getClassLoader().getResourceAsStream("words.txt");
         BufferedReader  rdr2 = new BufferedReader(new InputStreamReader(in2));
@@ -32,30 +41,28 @@ public class WordCount {
             }
         }
 
-        int cpus = Runtime.getRuntime().availableProcessors();
-        List<WordCountThread> workers = new ArrayList<>();
-        int fromIndex;
-        int toIndex = 0;
-
-        //Создаем потоки, передаем им участки книги и сет
         for(int i = 1; i <= cpus; i++){
-            fromIndex = toIndex;
-            if( i == cpus){
-                toIndex = lines.size();
-            }else{
-                toIndex = lines.size() - lines.size()*(cpus - i)/cpus;
-            }
-            WordCountThread wct = new WordCountThread(lines.subList(fromIndex, toIndex),set);
+//
+            WordCountThread wct = new WordCountThread(linesQueue, set);
             workers.add(wct);
         }
 
-        // запускаем потоки
+
+
         for(WordCountThread worker : workers){
             worker.start();
+        }
+        linesQueue.addAll(lines);
+        for (int i = 0; i < cpus; i++) {
+            linesQueue.add(STOP);
         }
         for(WordCountThread worker : workers){
             worker.join();
         }
+        for(WordCountThread worker : workers){
+            worker.uniteMapsWords();
+        }
+
 
         treeMap.putAll(result);
         int countWords = 0;
@@ -69,19 +76,20 @@ public class WordCount {
                 break;
             }
         }
+
         System.out.println("------------------------------");
         long finish = System.currentTimeMillis();
         System.out.println("Время выполнения : " + ((finish - start)/1000.0) + " секунд");
     }
 
     private static class WordCountThread extends Thread{
-        private  List<String> words;
+        private  BlockingQueue<String> linesQueue;
         private HashMap<String, Integer> wordCnt;
         private Set<String> excWords;
 
 
-        public WordCountThread(List<String> words, Set<String> excWords) {
-            this.words = words;
+        public WordCountThread(BlockingQueue<String> linesQueue, Set<String> excWords ) {
+            this.linesQueue = linesQueue;
             this.wordCnt = new HashMap<>();
             this.excWords = excWords;
         }
@@ -89,34 +97,41 @@ public class WordCount {
 
         @Override
         public void run() {
-            uniteMapsWords(splitLines(words, excWords));
+            while ((!isInterrupted())){
+                try {
+                    String line = linesQueue.take();
+                    if (line == STOP){
+                        break;
+                    }
+
+                    splitLines(line, excWords);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
         }
 
-        // Метод собирает карту без символов, не нужных слов (предлоги и тд, который помещены в сет из файла words)
-        private HashMap<String, Integer> splitLines(List<String> words, Set<String> excWords){
-            outer:for(String word : words) { // Считываем строку
-                String[] arr = word.replaceAll("[^a-zA-Z]", " ").
-                        trim().replaceAll("\\s+", " ").split(" "); //Делим строку на слова
-                if (arr[0].trim().isEmpty()) { // проверяем является ли строка пустой
+        private HashMap<String, Integer> splitLines(String line, Set<String> excWords){
+            String[] arr = line.replaceAll("[^a-zA-Z]", " ").
+                    trim().replaceAll("\\s+", " ").split(" "); //Делим строку на слова
+            outer:for(String word : arr){
+                if (word.trim().isEmpty()) { // проверяем является ли строка пустой
                     continue outer;
                 }
-                for(String s : arr){ // проверяем на совпадение слов со словами из сета и затем добавляем в карту
-                    if(!excWords.contains(s)){
-                        if (wordCnt.containsKey(s)) { // сли слово в карте уже есть, увеличиваем значение
-                            wordCnt.put(s.toLowerCase(), wordCnt.get(s) + 1);
-                        } else {
-                            wordCnt.put(s, 1);
-                        }
+                if(!excWords.contains(word)){
+                    if (wordCnt.containsKey(word)) { // сли слово в карте уже есть, увеличиваем значение
+                        wordCnt.put(word.toLowerCase(), wordCnt.get(word) + 1);
+                    } else {
+                        wordCnt.put(word, 1);
                     }
                 }
             }
             return wordCnt;
         }
-
-        // Каждый поток по очереди добавляет свои карты в результирующую,
-        // для повторяющихся слов значения суммируются
-        private synchronized void uniteMapsWords(HashMap<String, Integer> wordCnt){
-            for(Map.Entry<String, Integer> entry : wordCnt.entrySet()){
+        private void uniteMapsWords(){
+            for(Map.Entry<String, Integer> entry : this.wordCnt.entrySet()){
                 String word = entry.getKey();
                 Integer value = entry.getValue();
                 if(result.containsKey(word)){
